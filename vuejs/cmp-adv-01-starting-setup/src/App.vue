a8521d79ebf457ec5d3585b995c03f41eec48a88
remidatroa <seyazu@vegeta.local>
2022-09-16 14:05:29 +0200

    scoped slot and slotProps, displayed in the parents as object

--- vuejs/cmp-adv-01-starting-setup/src/App.vue
+++ vuejs/cmp-adv-01-starting-setup/src/App.vue
@@ -7,6 +7,11 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals>
+      <template #default="slotProps">
+        <h2>{{slotProps.item}}</h2>
+      </template>
+    </course-goals>
   </div>
 </template>
 
@@ -14,13 +19,14 @@
   import TheHeader from './components/TheHeader.vue';
   import BadgeList from './components/BadgeList.vue';
   import UserInfo from './components/UserInfo.vue';
-  
+  import CourseGoals from './components/CourseGoals.vue';
 
   export default {
     components:{
       TheHeader,
       BadgeList,
-      UserInfo
+      UserInfo,
+      CourseGoals
     },
   data() {
     return {
---
+++ vuejs/cmp-adv-01-starting-setup/src/components/CourseGoals.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+    <li v-for="goal in goals" :key="goal">
+        <slot :item="goal"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            goals: ['Build app', 'learn new frameworks']
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
