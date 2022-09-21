c16a3c0599babf24109b60cbe0b9fd8f43f87c86
remidatroa <seyazu@vegeta.local>
2022-09-21 13:14:23 +0200

    local comp alert added ErrorAlert and confirmAlet and OPEN attribut in the errorAlertComp

---
+++ vuejs/cmp-adv-01-starting-setup/src/components/ErrorAlert.vue
@@ -0,0 +1,18 @@
+<template>
+    <dialog open>
+        <slot></slot>
+    </dialog>
+</template>
+<style  scoped>
+    dialog{
+        margin: 0px;
+        position: fixed;
+        top: 20vh;
+        left: 30%;
+        width: 40%;
+        background-color: brown;
+        color: aliceblue;
+        padding: 15px;
+    }
+
+</style>
