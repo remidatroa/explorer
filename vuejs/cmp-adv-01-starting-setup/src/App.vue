33d6ee38237a54bc61ce6d9b31c7a7163e9f39e1
remidatroa <seyazu@vegeta.local>
2022-09-21 12:42:45 +0200

    dynamic components added and keep-alive statement

--- vuejs/cmp-adv-01-starting-setup/src/App.vue
+++ vuejs/cmp-adv-01-starting-setup/src/App.vue
@@ -2,34 +2,33 @@
   <div>
     <the-header></the-header>
     <!-- <TheHeader /> -->
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-    <course-goals #default="slotProps">
-      <h2>{{ slotProps.item }}</h2>
-      <p>{{ slotProps ['anotherProp'] }}</p>
-    </course-goals>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <Active-goals v-if="selectedComponent === 'active-goals'"></Active-goals>
+    <Manage-Goals v-if="selectedComponent === 'manage-goals'"></Manage-Goals> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoals from './components/CourseGoals.vue';
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+// import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 
 export default {
   components: {
     TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoals,
+    ActiveGoals,
+    ManageGoals
   },
   data() {
     return {
+      selectedComponent:'active-goals',
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
@@ -37,6 +36,11 @@ export default {
       },
     };
   },
+  methods: {
+    setSelectedComponent (cmp){
+      this.selectedComponent = cmp;
+    }
+  }
 };
 </script>
 
