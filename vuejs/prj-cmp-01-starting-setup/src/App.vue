dbb3c22015b7c31ce8b8cc2a82423fb3682575eb
remidatroa <seyazu@vegeta.local>
2022-10-03 13:28:45 +0100

    setup app data and first component

---
+++ vuejs/prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <ul>
+        <Learning-resource v-for="res in storedResources" 
+        :key="res.id" 
+        :title="res.title" 
+        :description="res.description" 
+        :link="res.link">
+
+        </Learning-resource>
+    </ul>
+</template>
+<script>
+    import LearningResource from './components/LearningResource.vue'
+
+    export default{
+        components:{
+            LearningResource
+        },
+
+        data() {
+            return {
+                storedResources: [
+                    { 
+                        id:'official-guide', 
+                        title: 'Official Guide',
+                        description: 'The official Vuejs docummentation.',
+                        link: 'https://vuejs.org'
+                    },
+                    {
+                        id: 'google',
+                        title: 'Google',
+                        description: 'Learn to google...',
+                        link: 'https://google.com'
+                    },
+                ]
+            };
+        }
+    }
+</script>
