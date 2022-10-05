88c0bb1c08208fa27a74eb9b461ff8d0b3c52dfd
remidatroa <seyazu@vegeta.local>
2022-10-05 13:21:05 +0100

    adding a compHEader and a general baseCard

--- vuejs/prj-cmp-01-starting-setup/src/App.vue
+++ vuejs/prj-cmp-01-starting-setup/src/App.vue
@@ -1,20 +1,15 @@
 <template>
-    <ul>
-        <Learning-resource v-for="res in storedResources" 
-        :key="res.id" 
-        :title="res.title" 
-        :description="res.description" 
-        :link="res.link">
-
-        </Learning-resource>
-    </ul>
+        <the-header title="RememberMe"></the-header>
+        <stored-resources :resources="storedResources"></stored-resources>
+    
 </template>
 <script>
-    import LearningResource from './components/LearningResource.vue'
-
+    import StoredResources from './components/learning-resources/StoredResources.vue';
+    import TheHeader from './components/layouts/TheHeader.vue';
     export default{
         components:{
-            LearningResource
+            StoredResources,
+            TheHeader,
         },
 
         data() {
@@ -37,3 +32,18 @@
         }
     }
 </script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: 'Roboto', sans-serif;
+}
+
+body {
+    margin: 0;
+}
+</style>
